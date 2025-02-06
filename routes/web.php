ed411bea6dda1fb69aef6cce5123cbd779daf657
Gabriel Ladeira <gabrielxladeira@gmail.com>
2025-02-06 09:10:26 -0300

    add cake routes and its tests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CakeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('api/v1')
+    ->group(function () {
+        Route::prefix('cakes')
+            ->name('cakes')
+            ->group(function () {
+                Route::get('/', [CakeController::class, 'list']);
+                Route::get('/{id}', [CakeController::class, 'show']);
+                Route::post('/', [CakeController::class, 'store']);
+                Route::put('{id}', [CakeController::class, 'update']);
+                Route::delete('{id}', [CakeController::class, 'destroy']);
+                Route::get('{id}/orders', [CakeController::class, 'orders']);
+            });
+    });
