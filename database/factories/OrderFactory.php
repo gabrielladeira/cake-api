42b3c41d52b4858853404e6c762024a849bddbcc
Gabriel Ladeira <gabrielxladeira@gmail.com>
2025-02-06 09:15:59 -0300

    add order factory

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Cake;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'email' => $this->faker->email,
+            'cake_id' => Cake::factory('')->create()->id
+        ];
+    }
+}
