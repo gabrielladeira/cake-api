0b00abb21c3b6032c583237bd909ff2c68b2318f
Gabriel Ladeira <gabrielxladeira@gmail.com>
2025-02-06 09:13:47 -0300

    disable csrf validation on api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->use(
+            [
+                \Illuminate\Foundation\Http\Middleware\TrimStrings::class
+            ]);
+        
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
